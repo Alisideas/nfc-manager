2a90c30c1f0317f5f783f8b06034aa0d55be9089
Alireza Akbari <alirezajj118@gmail.com>
2025-05-17 19:07:34 +0300

    fix count patients and recent bugs

--- app/api/admin/patients/count/route.ts
+++ app/api/admin/patients/count/route.ts
@@ -1,8 +1,18 @@
 // app/api/admin/patients/count/route.ts
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
+import getCurrentUser from "@/app/actions/getCurrentUser";
 
 export async function GET() {
-  const count = await prisma.patient.count();
+  const currentUser = await getCurrentUser();
+  if (!currentUser) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+  const count = await prisma.patient.count({
+    where: {
+      userId: currentUser.id,
+    },
+  });
+
   return NextResponse.json({ count });
 }
--- app/api/admin/patients/recent/route.ts
+++ app/api/admin/patients/recent/route.ts
@@ -1,10 +1,18 @@
-import  prisma from "@/lib/prisma";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  const currentUser = await getCurrentUser();
+  if (!currentUser) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
   const recentPatients = await prisma.patient.findMany({
     orderBy: { createdAt: "desc" },
     take: 5,
+    where: {
+      userId: currentUser.id,
+    },
     select: {
       id: true,
       firstName: true,
